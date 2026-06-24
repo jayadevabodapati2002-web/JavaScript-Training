f5e09e515b5badb4d3d8d54f80c31c168c830767
jayadevabodapati2002-web <jayadevabodapati2002@gmail.com>
2026-06-24 15:06:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+// Task 1 - Employee Name
+let name = prompt("Employee Name")
+
+console.log(name)
+
+document.writeln("Welcome : ",name)
+
+
+// Task 2 - Company Entry Confirmation
+
+let UserInfo = confirm("Are you ready to join today's JavaScript training?")
+
+console.log(UserInfo)
+
+
+// Task 3 - Customer Greeting
+
+let User = alert("Welcome to Stackly Solutions!")
+
+console.log("Customer Entered the Website.")
+
+
+// Task 4 - Student Details
+
+let UserName = prompt("Name");
+
+let UserAge = prompt("Age");
+
+console.log(UserName);
+
+console.log(UserAge);
+
+
+// Task 5 - Product Price
+
+let ProductPrice = 200
+
+ProductPrice = 100
+
+console.log(ProductPrice)
+
+
+// Task 6 Login Validation
+
+let username = prompt("Enter your username:");
+
+if (username === "") {
+    console.warn("Warning: Username is empty.");
+} else {
+    console.log("Login Successful: " + username);
+}
+
+// Task 7 - Website Maintenance
+
+let a = alert("Website is under maintenance")
+
+console.error("Error: Website is currently unavailable.")
+
+
+// Task 8 - Feedback Collection
+
+let feedback = prompt("How was today's JavaScript session?")
+
+console.log(feedback)
+
+document.writeln("Thank you for your feedback!")
+
+
+// Task 9 - Profile Information
+
+let Name = prompt("Enter Your Name:")
+
+let City = prompt("Enter Your City:")
+
+let Language = prompt("Enter Your Favourite Programming Language:")
+
+console.log("Name : " + Name)
+
+console.log("City : " + City)
+
+console.log("Favorite Programming Language : " + Language)
+
+// Task 10 - Mini Registration Form
+
+let fullname = prompt("Enter Full Name:");
+
+let email = prompt("Enter Email:");
+
+let mobile = prompt("Enter Mobile Number:");
+
+console.log("===== Registration Details =====");
+console.log("Name   : " + fullname);
+console.log("Email  : " + email);
+console.log("Mobile : " + mobile);
+
+alert("Registration Successfull")
+
+
+
+
+
